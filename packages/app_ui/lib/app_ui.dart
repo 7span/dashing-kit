8c629d2c8d3e4d231529b62e88c513b9fb438a9d
urvashikharecha <urvashi.k@7span.com>
2024-07-29 11:53:26 +0530

    - App run issue fixes for null aware operator - In Dropdown add bool in required builders

--- packages/app_ui/lib/app_ui.dart
+++ packages/app_ui/lib/app_ui.dart
@@ -1,4 +1,4 @@
-export 'src/theme/theme.dart';
-export 'src/widgets/widgets.dart';
-export 'src/widgets/animation/app_animation.dart';
 export 'src/gen/assets.gen.dart';
+export 'src/theme/theme.dart';
+export 'src/widgets/animation/app_animation.dart';
+export 'src/widgets/widgets.dart';
