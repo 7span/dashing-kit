15830b49afef5c5f23533682171b0443e601ceb7
Avni <avni.p@7span.com>
2025-01-31 12:38:20 +0530

    - Updated melos run configurations to use `SCRIPT_TEXT` instead of `SCRIPT_PATH` and `SCRIPT_OPTIONS`. - Added in_app_purchase_storekit plugin to `GeneratedPluginRegistrant.swift` on macOS. - Changed sqflite to sqflite_darwin in `GeneratedPluginRegistrant.swift`.

--- apps/app_core/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/app_core/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,14 +9,16 @@ import device_info_plus
 import file_selector_macos
 import firebase_core
 import firebase_crashlytics
+import in_app_purchase_storekit
 import path_provider_foundation
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
+  InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
