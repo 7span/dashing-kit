74f0828eb066248d1342678dbd74ce499f120b9c
Cavin <cavinmacwan1@gmail.com>
2025-03-19 19:12:11 +0530

    Update environment configuration and app icons; change launch arguments for development

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,6 @@ echo "Generating env config files"
 
 cat <<EOT >> apps/app_core/.env.dev
 BASE_API_URL=https://reqres.in/api/
-ONESIGNAL_APP_ID=fff612dc-7c9d-4e2b-94ff-cb5050d7c1c2
 BASE_API_KEY=https://jsonplaceholder.typicode.com/ 
 ENV=Development
 EOT
@@ -10,14 +9,12 @@ EOT
 
 cat <<EOT >> apps/app_core/.env.prod
 BASE_API_URL=https://reqres.in/api/
-ONESIGNAL_APP_ID=fff612dc-7c9d-4e2b-94ff-cb5050d7c1c2
 BASE_API_KEY=https://jsonplaceholder.typicode.com/ 
 ENV=Production
 EOT
 
 cat <<EOT >> apps/app_core/.env.staging
 BASE_API_URL=https://reqres.in/api/
-ONESIGNAL_APP_ID=fff612dc-7c9d-4e2b-94ff-cb5050d7c1c2
 BASE_API_KEY=https://jsonplaceholder.typicode.com/ 
 ENV=Staging
 EOT
