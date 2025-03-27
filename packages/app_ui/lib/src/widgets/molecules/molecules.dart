5d69d8f87727a47d8e3668dc338b73b49f250952
urvashikharecha <urvashi.k@7span.com>
2025-03-27 12:22:51 +0530

    - Added `AppCircularProgressIndicator` widget. - Added `WebViewScreen` for displaying web pages. - Updated `app_core` dependency to include `webview_flutter`. - Updated the home screen to use `AppCircularProgressIndicator`. - Exported `AppCircularProgressIndicator` in `molecules.dart`. - Added a new route for `WebViewScreen`.

--- packages/app_ui/lib/src/widgets/molecules/molecules.dart
+++ packages/app_ui/lib/src/widgets/molecules/molecules.dart
@@ -1,6 +1,7 @@
-export 'app_dropdown.dart';
-export 'app_textfield.dart';
-export 'app_button.dart';
 export 'app_bar.dart';
+export 'app_button.dart';
+export 'app_circular_progress_indicator.dart';
+export 'app_dropdown.dart';
 export 'app_network_image.dart';
 export 'app_refresh_indicator.dart';
+export 'app_textfield.dart';
