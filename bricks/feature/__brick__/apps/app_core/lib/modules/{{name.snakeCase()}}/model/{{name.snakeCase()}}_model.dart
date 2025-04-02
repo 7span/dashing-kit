60f5c09d4e506ec8f664524a13b84d741578567f
Avni <avni.p@7span.com>
2025-04-02 22:39:47 +0530

    imp: Refactored bloc structure by removing cubit and state files, and added api call with pagination.

--- bricks/feature/__brick__/apps/app_core/lib/modules/{{name.snakeCase()}}/model/{{name.snakeCase()}}_model.dart
+++ bricks/feature/__brick__/apps/app_core/lib/modules/{{name.snakeCase()}}/model/{{name.snakeCase()}}_model.dart
@@ -1,12 +1,74 @@
-class {{name.pascalCase()}}Model {
-  {{name.pascalCase()}}Model({required this.id});
+import 'package:app_core/core/data/models/pagination_model.dart';
 
-  factory {{name.pascalCase()}}Model.fromJson(Map<String, dynamic> json) {
-    return {{name.pascalCase()}}Model(id: json['id'] as String);
+class {{name.pascalCase()}}ResponseModel {
+  const {{name.pascalCase()}}ResponseModel(this.{{name.camelCase()}});
+
+  factory {{name.pascalCase()}}ResponseModel.fromJson(Map<String, dynamic> json) {
+    return {{name.pascalCase()}}ResponseModel({{name.pascalCase()}}.fromJson(json['{{name.camelCase()}}s']));
   }
-  final String id;
 
-  Map<String, dynamic> toJson({{name.pascalCase()}}Model {{name.snakeCase()}}Model) {
-    return {'id': {{name.snakeCase()}}Model.id};
+  final {{name.pascalCase()}}? {{name.camelCase()}};
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    if ({{name.camelCase()}} != null) {
+      data['{{name.camelCase()}}'] = {{name.camelCase()}}!.toJson();
+    }
+    return data;
+  }
+}
+
+class {{name.pascalCase()}} {
+  {{name.pascalCase()}}({this.data, this.pagination});
+
+  {{name.pascalCase()}}.fromJson(dynamic json) {
+    if (json['data'] != null) {
+      data = <{{name.pascalCase()}}Model>[];
+      // ignore: inference_failure_on_untyped_parameter
+      json['data'].forEach((v) {
+        data!.add({{name.pascalCase()}}Model.fromJson(v));
+      });
+    }
+    pagination =
+        json['pagination'] != null
+            ? PaginationModel.fromJson(json['pagination'])
+            : null;
+  }
+
+  List<{{name.pascalCase()}}Model>? data;
+  PaginationModel? pagination;
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    if (pagination != null) {
+      data['pagination'] = pagination!.toJson();
+    }
+    return data;
+  }
+}
+
+// ignore: must_be_immutable
+class {{name.pascalCase()}}Model {
+  {{name.pascalCase()}}Model({this.dataId, this.title, this.description});
+
+  factory {{name.pascalCase()}}Model.fromJson(dynamic json) => {{name.pascalCase()}}Model(
+    dataId: json['id'] as int?,
+    title: json['title'] as String?,
+    description: json['description'] as String?,
+  );
+
+  int? dataId;
+  String? title;
+  String? description;
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['id'] = dataId;
+    data['title'] = title;
+    data['description'] = description;
+    return data;
   }
 }
--- bricks/feature/__brick__/apps/app_core/lib/modules/{{name.snakeCase()}}/screen/{{name.snakeCase()}}_screen.dart
+++ bricks/feature/__brick__/apps/app_core/lib/modules/{{name.snakeCase()}}/screen/{{name.snakeCase()}}_screen.dart
@@ -1,16 +1,168 @@
+import 'package:api_client/api_client.dart';
+import 'package:app_core/app/helpers/extensions/extensions.dart';
+import 'package:app_core/app/helpers/mixins/pagination_mixin.dart';
+import 'package:app_core/modules/{{name.snakeCase()}}/bloc/{{name.snakeCase()}}_bloc.dart';
+import 'package:app_core/modules/{{name.snakeCase()}}/repository/{{name.snakeCase()}}_repository.dart';
 import 'package:app_ui/app_ui.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 @RoutePage()
-class {{name.pascalCase()}}Screen extends StatelessWidget {
-  const {{name.pascalCase()}}Screen ({super.key});
+class {{name.pascalCase()}}Screen extends StatefulWidget implements AutoRouteWrapper {
+  const {{name.pascalCase()}}Screen({super.key});
 
-   @override
+  @override
+  Widget wrappedRoute(BuildContext context) {
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<{{name.pascalCase()}}Repository>(
+          create: (context) => const {{name.pascalCase()}}Repository(),
+        ),
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create:
+                (context) => {{name.pascalCase()}}Bloc(
+                  RepositoryProvider.of<{{name.pascalCase()}}Repository>(context),
+                )..safeAdd(const Get{{name.pascalCase()}}Event()),
+          ),
+        ],
+        child: this,
+      ),
+    );
+  }
+
+  @override
+  State<{{name.pascalCase()}}Screen> createState() => _{{name.pascalCase()}}ScreenState();
+}
+
+class _{{name.pascalCase()}}ScreenState extends State<{{name.pascalCase()}}Screen>
+    with PaginationService {
+  late final ScrollController pageScrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageScrollController = scrollController;
+  }
+
+  @override
+  void dispose() {
+    pageScrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return AppScaffold(
-      appBar: AppBar(title: const Text('{{name.pascalCase()}}')),
-      body: const Center(child: Text('{{name.pascalCase()}} Screen')),
+      appBar: const CustomAppBar(title: '{{name.pascalCase()}}', leading: BackButton()),
+      body: BlocBuilder<{{name.pascalCase()}}Bloc, {{name.pascalCase()}}State>(
+        buildWhen:
+            (previous, current) => previous.apiStatus != current.apiStatus,
+        builder: (context, state) {
+          switch (state.apiStatus) {
+            case ApiStatus.initial:
+            case ApiStatus.loading:
+              return const AppCircularProgressIndicator();
+            case ApiStatus.empty:
+              return EmptyScreen(
+                showButton: false,
+                title: 'No meowTicket was found!',
+                subTitle:
+                    'Keep yourself reminded of your tasks, so no pending task goes unnoticed!',
+                buttonTitle: 'Create {{name.pascalCase()}}',
+                onTap: () {},
+              );
+            case ApiStatus.error:
+              return const Center(child: Text('Error'));
+            case ApiStatus.loaded:
+              return _{{name.pascalCase()}}View(scrollController: pageScrollController);
+          }
+        },
+      ),
+    );
+  }
+
+  @override
+  Future<void> onEndScroll() async {
+    context.read<{{name.pascalCase()}}Bloc>().safeAdd(const LoadMore{{name.pascalCase()}}Event());
+  }
+}
+
+class _{{name.pascalCase()}}View extends StatelessWidget {
+  const _{{name.pascalCase()}}View({required this.scrollController});
+
+  final ScrollController scrollController;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<{{name.pascalCase()}}Bloc, {{name.pascalCase()}}State>(
+      builder: (context, state) {
+        return AppRefreshIndicator(
+          onRefresh:
+              () async => context.read<{{name.pascalCase()}}Bloc>().add(
+                const Get{{name.pascalCase()}}Event(),
+              ),
+          child: ListView.separated(
+            physics: const AlwaysScrollableScrollPhysics(),
+            controller: scrollController,
+            itemCount:
+                !state.hasMorePages || (state.apiStatus == ApiStatus.loading)
+                    ? state.{{name.camelCase()}}List!.length
+                    : state.{{name.camelCase()}}List!.length + 1,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Insets.medium16,
+              vertical: Insets.medium16,
+            ),
+            itemBuilder: (context, index) {
+              ///! [!widget.isLoading] This condition is there because we don't need to show the indicator
+              ///! when the user is seeing the shimmer effect for the fist time
+              if (index >= state.{{name.camelCase()}}List!.length &&
+                  (state.apiStatus == ApiStatus.loading)) {
+                return const Center(
+                  child: AppPadding.regular(
+                    child: SizedBox(
+                      height: 24,
+                      width: 24,
+                      child: CircularProgressIndicator(strokeWidth: 1.5),
+                    ),
+                  ),
+                );
+              } else {
+                final model = state.{{name.camelCase()}}List![index];
+                return {{name.pascalCase()}}ListTile(
+                  id: model.dataId.toString(),
+                  onTap: () {},
+                );
+              }
+            },
+            separatorBuilder: (context, index) => VSpace.xsmall8(),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class {{name.pascalCase()}}ListTile extends StatelessWidget {
+  const {{name.pascalCase()}}ListTile({required this.id, required this.onTap, super.key});
+
+  final String id;
+  final VoidCallback onTap;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedGestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(Insets.small12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(AppBorderRadius.small8),
+          color: context.colorScheme.white,
+        ),
+        child: AppText.subTitle10(text: id),
+      ),
     );
   }
 }
