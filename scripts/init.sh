cd536803aa99e52d23edb945a8b244f130b58d23
Avni <avni.p@7span.com>
2025-04-09 10:19:09 +0530

    Imp: update API base URLs in environment configuration files for development, production, and staging

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,18 +3,21 @@
 echo "Generating env config files"
 
 cat <<EOT >> apps/app_core/.env.dev
-BASE_API_URL=https://reqres.in/api/
+BASE_API_URL=https://jsonplaceholder.typicode.com/
+USER_API_URL=https://reqres.in/
 ENV=Development
 EOT
 
 
 cat <<EOT >> apps/app_core/.env.prod
-BASE_API_URL=https://reqres.in/api/
+BASE_API_URL=https://jsonplaceholder.typicode.com/
+USER_API_URL=https://reqres.in/
 ENV=Production
 EOT
 
 cat <<EOT >> apps/app_core/.env.staging
-BASE_API_URL=https://reqres.in/api/
+BASE_API_URL=https://jsonplaceholder.typicode.com/
+USER_API_URL=https://reqres.in/
 ENV=Staging
 EOT
 
